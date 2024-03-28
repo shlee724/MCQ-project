23108501fb26b77cc11e6dff2258a590f25aca8b
paperman1 <powerplant724@gmail.com>
2024-03-28 21:39:05 +0900

    Added pdf_to_excel_using_pdfplumber.py

---
+++ pdf_to_excel_using_pdfplumber.py
@@ -0,0 +1,21 @@
+import os  
+import pdfplumber  
+import pandas as pd  
+
+
+file_path = os.getcwd() 
+pdf_name = "example.pdf"  
+pdf_file_path = file_path + pdf_name  
+
+# PDF 파일 열기  
+pdf = pdfplumber.open(pdf_file_path)  
+
+pages = pdf.pages  
+print("총 페이지 수 : ", len(pages))  
+
+tables = []  
+
+# 1. 페이지에서 표 데이터 추출하기  
+for each in pages:  
+    table = each.extract_tables()  
+    tables.extend(table)
