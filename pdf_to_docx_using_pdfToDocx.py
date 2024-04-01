87c6d82951f6ac76bd067a6762997e92b3c5fa86
paperman1 <powerplant724@gmail.com>
2024-04-01 21:51:32 +0900

    Added pdf_to_docx_using_pdfToDocx.py

---
+++ pdf_to_docx_using_pdfToDocx.py
@@ -0,0 +1,15 @@
+from pdf2docx import Converter
+
+# PDF 파일 경로
+pdf_file = 'your_pdf_file.pdf'
+# 변환된 Word 파일 경로
+word_file = 'converted_word_file.docx'
+
+# Converter 객체 생성
+cv = Converter(pdf_file)
+
+# PDF를 Word로 변환
+cv.convert(word_file)
+
+# 변환 프로세스 종료
+cv.close()
